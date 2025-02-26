34d982771da8cfd55a8f3946c3cd0770b1d53abf
el4abdu <mmmmorad123@gmail.com>
2025-02-26 15:10:19 +0000

    Auto update 20250226-151019

--- first_time_setup.bat
+++ first_time_setup.bat
@@ -1,15 +1,20 @@
 @echo off
 echo Setting up repository for first time...
 
-:: Clone the repository
-git clone https://github.com/el4abdu/al-en777.git
-cd al-en777
-
 :: Set git credentials
 git config --global user.name "el4abdu"
 git config --global user.email "mmmmorad123@gmail.com"
 
+:: Initialize git repository
+git init
+
+:: Add remote repository
+git remote add origin https://github.com/el4abdu/al-en777.git
+
+:: Create main branch
+git checkout -b main
+
 echo.
-echo Setup Complete!
+echo Setup Complete! Now you can use update_github.bat to push changes
 echo.
 pause 
--- update_github.bat
+++ update_github.bat
@@ -1,10 +1,6 @@
 @echo off
 echo Updating GitHub Repository...
 
-:: Set git credentials (only needed first time)
-git config --global user.name "el4abdu"
-git config --global user.email "mmmmorad123@gmail.com"
-
 :: Add all changes
 git add .
 
@@ -15,8 +11,8 @@ set datetime=%datetime:~0,8%-%datetime:~8,6%
 :: Commit changes with timestamp
 git commit -m "Auto update %datetime%"
 
-:: Push to GitHub
-git push origin master
+:: Push to GitHub (using main branch)
+git push -u origin main
 
 echo.
 echo Update Complete!
